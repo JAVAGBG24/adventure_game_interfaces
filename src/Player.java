f50037f8224308c8643cc4d65a7196474bb0cfc8
Helena <helena@klarr.se>
2024-10-14 09:58:48 +0200

    init commit

---
+++ src/Player.java
@@ -0,0 +1,37 @@
+public class Player {
+/*    private String name;
+    private int health;
+    private Room currentRoom;
+
+    public Player(String name) {
+        this.name = name;
+        this.health = 100;
+    }
+
+    public void moveTo(Room room) {
+        this.currentRoom = room;
+        System.out.println("Du går in i " + room.getName() + ".");
+        System.out.println(room.getDescription());
+        room.showInteractables();
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void changeHealth(int amount) {
+        this.health += amount;
+        System.out.println("Din hälsa är nu " + health + ".");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Room getCurrentRoom() {
+        return currentRoom;
+    }*/
+}
+
+
+
